9857792bcc09b934e561a66773e20078a32a517b
Iric <iricbing@gmail.com>
2018-12-18 16:06:16 +0800

    Initialize XPrinter project.

---
+++ src/android/XPrinter.java
@@ -0,0 +1,142 @@
+package cn.iricbing.xprinter;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import static net.posprinter.utils.DataForSendToPrinterPos80.selectAlignment;
+import static net.posprinter.utils.DataForSendToPrinterPos80.selectCharacterSize;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class XPrinter extends CordovaPlugin {
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    private BluetoothDevice mBluetoothDevice = null;
+    private BluetoothSocket mBluetoothSocket = null;
+    private OutputStream mOutputStream = null;
+
+    private static final Integer SMALL_SIZE = 0;
+    private static final Integer NORMAL_SIZE = 17;
+    private static final Integer LARGE_SIZE = 34;
+
+    private static final Integer ALIGN_LEFT = 0;
+    private static final Integer ALIGN_CENTER = 1;
+    private static final Integer ALIGN_RIGHT = 2;
+
+    private static final UUID SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        switch (action) {
+        case "coolMethod": {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+        }
+            break;
+        case "connect": {
+            String macAddress = args.getString(0);
+            this.connect(macAddress, callbackContext);
+        }
+            break;
+        case "printTest": {
+            String title = args.getString(0);
+            String body = args.getString(1);
+            String footer = args.getString(2);
+            this.printTest(title, body, footer, callbackContext);
+        }
+            break;
+        }
+        return true;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+    private void connect(String macAddress, CallbackContext callbackContext) {
+        if (bluetoothAdapter == null) {
+            callbackContext.error("The current device does not support bluetooth");
+            return;
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            callbackContext.error("Bluetooth is not enabled");
+            return;
+        }
+        if (macAddress == null || macAddress.length() == 0) {
+            callbackContext.error("Invalidate MAC address " + macAddress);
+            return;
+        }
+        try {
+            mBluetoothDevice = bluetoothAdapter.getRemoteDevice(macAddress);
+            mBluetoothSocket = mBluetoothDevice.createRfcommSocketToServiceRecord(SPP_UUID);
+            mBluetoothSocket.connect();
+            callbackContext.success("Connect success");
+        } catch (Exception e) {
+            callbackContext.error("Connect error: " + e.toString());
+        }
+    }
+
+    private void printTest(String title, String body, String footer, CallbackContext callbackContext) {
+        try {
+            printTitle(title);
+            printBody(body);
+            printFooter(footer);
+            cutPaper();
+            callbackContext.success("Test print success");
+        } catch (Exception e) {
+            callbackContext.error("Test print error: " + e.toString());
+        }
+    }
+
+    private void printTitle(String title) throws IOException {
+        mOutputStream = mBluetoothSocket.getOutputStream();
+        byte[] sizeByte = selectCharacterSize(LARGE_SIZE);
+        mOutputStream.write(sizeByte);
+        byte[] aliginByte = selectAlignment(ALIGN_CENTER);
+        mOutputStream.write(aliginByte);
+        mOutputStream.write((title + "\n").getBytes("GBK"));
+        mOutputStream.flush();
+    }
+
+    private void printBody(String body) throws IOException {
+        mOutputStream = mBluetoothSocket.getOutputStream();
+        byte[] sizeByte = selectCharacterSize(NORMAL_SIZE);
+        mOutputStream.write(sizeByte);
+        byte[] aliginByte = selectAlignment(ALIGN_CENTER);
+        mOutputStream.write(aliginByte);
+        mOutputStream.write((body + "\n").getBytes("GBK"));
+        mOutputStream.flush();
+    }
+
+    private void printFooter(String footer) throws IOException {
+        mOutputStream = mBluetoothSocket.getOutputStream();
+        byte[] sizeByte = selectCharacterSize(SMALL_SIZE);
+        mOutputStream.write(sizeByte);
+        byte[] aliginByte = selectAlignment(ALIGN_LEFT);
+        mOutputStream.write(aliginByte);
+        mOutputStream.write((footer + "\n\n\n\n\n\n").getBytes("GBK"));
+        mOutputStream.flush();
+    }
+
+    private void cutPaper() throws IOException {
+        mOutputStream = mBluetoothSocket.getOutputStream();
+        mOutputStream.write(new byte[] { 0x0a, 0x0a, 0x1d, 0x56, 0x01 });
+        mOutputStream.flush();
+    }
+}
---
+++ www/XPrinter.js
@@ -0,0 +1,13 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'XPrinter', 'coolMethod', [arg0]);
+};
+
+exports.connect = function (arg0, success, error) {
+    exec(success, error, 'XPrinter', 'connect', [arg0]);
+};
+
+exports.printTest = function (title, body, footer, success, error) {
+    exec(success, error, 'XPrinter', 'printTest', [title, body, footer]);
+};
